09ac491936c0fccf977ca0f5cb913e991faf8517
RodyakinVladislav <rodyakin.vlad@mail.ru>
2023-12-24 23:50:10 +0700

    attempt1

--- Uniq_ptr/Uniq_ptr/test.cpp
+++ Uniq_ptr/Uniq_ptr/test.cpp
@@ -1,6 +1,120 @@
 #include "gtest/gtest.h"
+#include "uniq_ptr.h"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+struct test_struct {
+    int* _val;
+    test_struct(int* val) { _val = val; }
+    test_struct(const test_struct& other) {
+        _val = other._val;
+    }
+    ~test_struct() { _val = nullptr; }
+    void multiply(int times) {
+        if (_val) {
+            _val[0] *= times;
+        }
+        else {
+            return;
+        }
+    }
+};
+
+TEST(Constructor, Default) {
+    UniquePointer<int> ptr;
+    EXPECT_FALSE(ptr);
+    EXPECT_EQ(ptr.get(), nullptr);
+}
+
+TEST(Constructor, PointerToObject) {
+    int* p1 = new int[150];
+    UniquePointer<int> ptr(p1);
+    EXPECT_EQ(ptr.get(), p1);
+
+    test_struct* p2 = new test_struct(p1);
+    UniquePointer <test_struct> ptr2(p2);
+    EXPECT_EQ(ptr2.get(), p2);
+}
+
+TEST(Constructor, Move) {
+    test_struct* p2 = new test_struct(new int[10]);
+    UniquePointer<test_struct> ptr(p2);
+    EXPECT_TRUE(ptr);
+
+    UniquePointer <test_struct> ptr2(std::move(ptr));
+    EXPECT_TRUE(ptr2);
+    EXPECT_FALSE(ptr);
+}
+
+TEST(Assignment, Move) {
+    int* p1 = new int[10];
+    UniquePointer<int> ptr(p1);
+    int* p2 = new int[100];
+    UniquePointer <int> ptr2(p2);
+    ptr = std::move(ptr2);
+
+    EXPECT_EQ(ptr.get(), p2);
+    EXPECT_EQ(ptr2, NULL);
+}
+
+TEST(Assignment, PointerToObject) {
+    int* p1 = new int[100];
+    int* p2 = new int[10];
+    UniquePointer<int> sptr(p1);
+    sptr = p2;
+    EXPECT_EQ(sptr.get(), p2);
+
+    test_struct* sp1 = new test_struct(p1);
+    UniquePointer <test_struct> sptr1(sp1);
+    test_struct* sp2 = new test_struct(p2);
+    sptr1 = sp2;
+    EXPECT_EQ(sptr1.get(), sp2);
+}
+
+TEST(Operators, Dereference) {
+    UniquePointer <test_struct> ptr;
+    int* new_int = new int[10];
+    test_struct* p2 = new test_struct(new_int);
+
+    UniquePointer <test_struct> ptr2(p2);
+
+    EXPECT_EQ(ptr2->_val, new_int);
+    ptr2->multiply(0);
+    EXPECT_EQ(p2->_val[0], 0);
+
+
+}
+TEST(Release, NoErrors) {
+    int* new_int = new int[10];
+    test_struct* sp = new test_struct(new_int);
+    UniquePointer <test_struct> ptr(sp);
+    EXPECT_EQ(ptr.get(), sp);
+    ptr.release();
+    EXPECT_EQ(ptr.get(), nullptr);
+    UniquePointer <test_struct> ptr2(sp);
+    EXPECT_EQ(ptr2.get(), sp);
+}
+
+
+TEST(Reset, NoErrors) {
+    double* d = new double[100];
+    UniquePointer <double> dptr;
+    dptr.reset(d);
+    EXPECT_EQ(dptr.get(), d);
+}
+
+TEST(Swap, NoErrors) {
+    test_struct* sp = new test_struct(new int[100]);
+    UniquePointer <test_struct> ptr1(sp);
+    UniquePointer <test_struct> ptr2;
+    ptr2.swap(ptr1);
+    EXPECT_EQ(ptr2.get(), sp);
+    EXPECT_EQ(ptr1.get(), nullptr);
+}
+
+TEST(MakeShared, NoErrors) {
+    int* new_int = new int[10];
+    auto ptr = make_unique<test_struct>(new_int);
+    EXPECT_EQ(ptr.get()->_val, new_int);
+    auto ptr2 = make_unique<double>(100);
+    double t = 100;
+    EXPECT_EQ(*ptr2, t);
 }
--- Uniq_ptr/Uniq_ptr/uniq_ptr.h
+++ Uniq_ptr/Uniq_ptr/uniq_ptr.h
@@ -1,5 +1,66 @@
 #pragma once
+#include<memory>
+#include<utility>
 
-template<typename T> class uniq_ptr {
+template<class Type, class TDeleter = std::default_delete<Type>> class UniquePointer final{
+    Type* m_ptr;
+    using t_UniquePTR = UniquePointer<Type, TDeleter>;
+public:
+    UniquePointer() : UniquePointer{ nullptr } {};
+    explicit UniquePointer(Type* pObject) : m_ptr{ pObject } {};
+    UniquePointer(t_UniquePTR&& uniquePTR) noexcept : m_ptr{ uniquePTR.release() } {};
+    ~UniquePointer() {
+        if (m_ptr) {
+            delete m_ptr;
+        }
+    }
 
+    // Assignment.
+    UniquePointer& operator=(t_UniquePTR&& uniquePTR) noexcept {
+        if (this != &uniquePTR) {
+            reset(uniquePTR.release());
+        }
+        return *this;
+    }
+    UniquePointer& operator=(Type* pObject) {
+        reset(pObject);
+        return *this;
+    }
+
+    // Observers.
+    Type& operator*() const { return *m_ptr; } // Dereference the stored pointer.
+    Type* operator->() const { return m_ptr; } // Return the stored pointer.
+    Type* get() const { return m_ptr; } // Return the stored pointer.
+    TDeleter& get_deleter() { return TDeleter; }// Return a reference to the stored deleter.
+
+    // Return false if the stored pointer is null.
+    operator bool() const {
+        return m_ptr != nullptr;
+    }
+
+    // Modifiers.
+    Type* release() {
+        Type* old = m_ptr;
+        m_ptr = nullptr;
+        return old;
+        // return std::exchange(m_ptr, nullptr);
+    }
+
+    void reset(Type* pObject = nullptr, TDeleter deleter = {}) {
+        Type* old = m_ptr;
+        m_ptr = pObject;
+        // Type* old = std::exchange(m_ptr, pObject);
+        if (old) {
+            delete old;
+        }
+    }
+    void swap(t_UniquePTR& uniquePTR) { std::swap(this->m_ptr, uniquePTR.m_ptr); }// Exchange the pointer with another object.
+
+private: // Disable copy from lvalue.
+    UniquePointer(const t_UniquePTR&) = delete;
+    t_UniquePTR& operator=(const t_UniquePTR&) = delete;
 };
+
+template<class Type, class... Args> UniquePointer<Type> make_unique(Args &&...args) {
+    return UniquePointer<Type>(new Type(std::forward<Args>(args)...));
+}
