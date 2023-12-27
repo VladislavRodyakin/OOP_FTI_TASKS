b1c9602fa15becbac195cf589c706ab6a9c6abb7
RodyakinVladislav <rodyakin.vlad@mail.ru>
2023-12-27 13:21:24 +0700

    tmp

--- FIFO/FIFO/FIFO.h
+++ FIFO/FIFO/FIFO.h
@@ -9,10 +9,12 @@ class ImageFIFO final {
 	std::vector<unsigned int*> data_container;
 	std::vector<bool> ready_for_writing_state;
 	std::queue<void*> ready_for_read_blocks;
+	std::mutex mut;
+	/*
 	std::mutex getFree_mutex;
 	std::mutex addReady_mutex;
 	std::mutex getReady_mutex;
-	std::mutex addFree_mutex;
+	std::mutex addFree_mutex;*/
 public:
 	ImageFIFO(size_t blockSize, size_t maxBlocks) : m_block_size{ blockSize }, m_max_queue_size{ maxBlocks } {
 		data_container = std::vector<unsigned int*>(maxBlocks, nullptr);
@@ -28,7 +30,8 @@ public:
 
 	// метод, вызываемый потоком-писателем, для получения указателя на очередной свободный блок данных
 	void* getFree() {
-		const std::lock_guard<std::mutex> lock(getFree_mutex);
+		//const std::lock_guard<std::mutex> lock(getFree_mutex);
+		const std::lock_guard<std::mutex> lock(mut);
 		while (true) {
 			for (int i = 0; i < m_max_queue_size; i++) {
 				if (ready_for_writing_state[i] == true && data_container[i] == nullptr) {
@@ -41,7 +44,8 @@ public:
 
 	// метод, вызываемый потоком-писателем, помечающий, что найденный ранее блок данных заполнен и готов к передаче
 	void addReady(void* data) {
-		const std::lock_guard<std::mutex> lock(addReady_mutex);
+		//const std::lock_guard<std::mutex> lock(addReady_mutex);
+		const std::lock_guard<std::mutex> lock(mut);
 		for (int i = 0; i < m_max_queue_size; i++) {
 			if (data_container[i] == data) {
 				ready_for_read_blocks.push(data);
@@ -52,7 +56,8 @@ public:
 
 	// метод, вызываемый потоком-читателем, для получения указателя на очередной готовый блок данных
 	void* getReady() {
-		const std::lock_guard<std::mutex> lock(getReady_mutex);
+		//const std::lock_guard<std::mutex> lock(getReady_mutex);
+		const std::lock_guard<std::mutex> lock(mut);
 		while (true) {
 			unsigned int* tmp = (unsigned int*)ready_for_read_blocks.front();
 			if (tmp != nullptr) {
@@ -64,7 +69,8 @@ public:
 
 	// метод, вызываемый потоком-читателем, помечающий, что выданный ранее блок данных обработан, освобождён и может повторно использоваться очередью
 	void addFree(void* data) {
-		const std::lock_guard<std::mutex> lock(addFree_mutex);
+		//const std::lock_guard<std::mutex> lock(addFree_mutex);
+		const std::lock_guard<std::mutex> lock(mut);
 		for (int i = 0; i < m_max_queue_size; i++) {
 			if (data_container[i] == data) {
 				ready_for_writing_state[i] = true;
